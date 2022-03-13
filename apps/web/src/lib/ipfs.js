d2e6e13aa424f64a3f4d3b21cf59ca4f8e0f755c
Kayaba Attribution <jdavidgomezca@gmail.com>
2022-03-12 23:03:41 -0500

    Feat: upload JSON to pinata

---
+++ apps/web/src/lib/ipfs.js
@@ -0,0 +1,59 @@
+//imports needed for this function
+const axios = require('axios');
+const fs = require('fs');
+const FormData = require('form-data');
+
+const pinataApiKey = 'b5a177b8da6d89da0f9e'
+const pinataSecretApiKey = '91e7253b4c7746f3a5661f94a23affea0fb3b8ad663155c2ce1e18c99b09a3b1'
+
+export const pinFileToIPFS = () => {
+    const url = `https://api.pinata.cloud/pinning/pinFileToIPFS`;
+
+    //we gather a local file for this example, but any valid readStream source will work here.
+    let data = new FormData();
+    data.append('file', fs.createReadStream('./Ethlogo.png'));
+
+    //You'll need to make sure that the metadata is in the form of a JSON object that's been convered to a string
+    //metadata is optional
+    const metadata = JSON.stringify({
+        name: 'testname',
+        keyvalues: {
+            exampleKey: 'exampleValue'
+        }
+    });
+    data.append('pinataMetadata', metadata);
+
+    //pinataOptions are optional
+    const pinataOptions = JSON.stringify({
+        cidVersion: 0,
+        customPinPolicy: {
+            regions: [
+                {
+                    id: 'FRA1',
+                    desiredReplicationCount: 1
+                },
+                {
+                    id: 'NYC1',
+                    desiredReplicationCount: 2
+                }
+            ]
+        }
+    });
+    data.append('pinataOptions', pinataOptions);
+
+    return axios
+        .post(url, data, {
+            maxBodyLength: 'Infinity', //this is needed to prevent axios from erroring out with large files
+            headers: {
+                'Content-Type': `multipart/form-data; boundary=${data._boundary}`,
+                pinata_api_key: pinataApiKey,
+                pinata_secret_api_key: pinataSecretApiKey
+            }
+        })
+        .then(function (response) {
+            //handle response here
+        })
+        .catch(function (error) {
+            //handle error here
+        });
+};
--- apps/web/src/routes/mint.svelte
+++ apps/web/src/routes/mint.svelte
@@ -1,8 +1,67 @@
 
 
 <script>
-
+    /* import the ipfs-http-client library */
+    //import { create } from 'ipfs-http-client';
     import { xrpl } from "$lib/xrp.js";
+
+    // Example POST method implementation:
+    const pinataApiKey = 'b5a177b8da6d89da0f9e'
+    const pinataSecretApiKey = '91e7253b4c7746f3a5661f94a23affea0fb3b8ad663155c2ce1e18c99b09a3b1'
+
+        
+    const info = JSON.stringify(
+        {
+            "name": "test"
+        }
+    )
+
+    const pinJSONToIPFSwithFetch = async (JSONBody) => {
+        const url = `https://api.pinata.cloud/pinning/pinJSONToIPFS`;
+        console.log(JSONBody)
+        const response = await fetch(url, {
+            method: 'POST', // *GET, POST, PUT, DELETE, etc.
+            headers: {
+                'Content-Type': 'application/json',
+                pinata_api_key: pinataApiKey,
+                pinata_secret_api_key: pinataSecretApiKey
+            },
+            body: JSONBody
+        });
+      console.log(response.json())
+      //return response.json(); // parses JSON response into native JavaScript objects
+    }
+
+    async function pinFileToIPFS(img) {
+        const url = `https://api.pinata.cloud/pinning/pinJSONToIPFS`;
+        // see base64 img
+        console.log(img)
+        // create data from
+        let data = new FormData();
+        // add base64 img
+        data.append('file', img);
+
+        //metadata is optional
+        const metadata = JSON.stringify({
+        name: 'testname',
+        keyvalues: {
+            exampleKey: 'exampleValue'
+        }
+        });
+        data.append('pinataMetadata', metadata);
+
+
+        const response = await fetch(url, {
+            method: 'POST', 
+            headers: {
+                'Content-Type': `multipart/form-data; boundary=${data._boundary}`,
+                pinata_api_key: pinataApiKey,
+                pinata_secret_api_key: pinataSecretApiKey
+            },
+            body: data // body data type must match "Content-Type" header
+        });
+        return response.json(); // parses JSON response into native JavaScript objects
+    }
 
     //***************************
     //** Mint Token *************
@@ -43,10 +102,51 @@
           JSON.stringify(xrpl.getBalanceChanges(tx.result.meta), null, 2))
         client.disconnect()
     } //End of mintToken
+
+
+    let  avatar, fileinput;
+	
+	const onFileSelected =(e)=>{
+    let image = e.target.files[0];
+            let reader = new FileReader();
+            reader.readAsDataURL(image);
+            reader.onload = e => {
+                 avatar = e.target.result
+            };
+    }
 </script>
 
 <div class="mt-20">
     <div class="text-4xl font-bold py-5 text-center">Mint Your NFT in XRPL</div>
+
+    <div class="text-4xl font-bold py-5 text-center">Upload image</div>
+
+
+    <input style="display:none" type="file" accept=".jpg, .jpeg, .png" on:change={(e)=>onFileSelected(e)} bind:this={fileinput} >
+    <div class="card w-96 bg-base-100 shadow-xl">
+        <figure class="px-10 pt-10">
+            {#if avatar}
+            <img class="avatar" src="{avatar}" alt="d" />
+            {:else}
+            <img class="avatar" src="./uploadimage.gif" alt="" /> 
+            {/if}
+        </figure>
+        <div class="card-body items-center text-center">
+          <h2 class="card-title">Upload an Image</h2>
+          <p>Please use .jpg, .jpeg, and .png only</p>
+          <div class="card-actions">
+            {#if !avatar}
+            <button class="btn btn-primary" on:click={()=>{fileinput.click();}}>Upload</button>
+            {:else}
+            <!-- <button class="btn btn-primary" on:click={()=>{pinFileToIPFS(avatar)}}>Save to IPFS</button> -->
+            <button class="btn btn-primary" on:click={()=>{pinJSONToIPFSwithFetch(info)}}>Save to IPFS</button>
+
+            {/if}
+            
+          </div>
+        </div>
+    </div>
+
 
     <div class="card w-96 bg-base-100 shadow-xl">
         <figure class="px-10 pt-10">
@@ -59,5 +159,5 @@
             <button class="btn btn-primary" on:click={mintToken}>Buy Now</button>
           </div>
         </div>
-      </div>
+    </div>
 </div>
